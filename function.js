403b7eeb7bfdd43ce943357322ab40a28666b852
Joelle  Johnson <joellej221@gmail.com>
2023-10-31 15:04:10 -0400

    added fizzbuzz challenges

--- function.js
+++ function.js
@@ -3,7 +3,7 @@ function celsius_to_fahrenheit(degreesCelsius){
     return fahrenheit;
 }
 
-console.log(celsius_to_fahrenheit(32) + "\u00B0F");
+// console.log(celsius_to_fahrenheit(32) + "\u00B0F");
 
 // Function practice #2 below
 function hoursToMinutes(hours){
@@ -11,11 +11,49 @@ function hoursToMinutes(hours){
     return minutes;
 }
 
-console.log(hoursToMinutes(2));
+// console.log(hoursToMinutes(2));
 
 // function practice 3
 function areaOfATriangle(base, height){
     const area = (.5 * base * height);
     return area;
 }
-console.log(areaOfATriangle(3, 5));
+// console.log(areaOfATriangle(3, 5));
+
+
+/**
+ * The Problem
+ * Write a program that takes one number and print the number.
+ * If the number is multiple of 3 print “Fizz” instead of the number.
+ * If the number is multiple of 5 print “Buzz” instead of the number.
+ * If the number is multiple of both 3 and 5 print “FizzBuzz”.
+ */
+
+function fizzBuzz(num){
+    if(num % 3 === 0 && num % 5 === 0){
+        console.log("FizzBuzz")
+    }else if(num % 3 === 0 && num % 5 !== 0){
+        console.log("Fizz")
+    } else if (num % 5 === 0 && num % 3 !== 0) {
+        console.log("Buzz")
+    } else {
+        console.log('No fizzbuzz')
+    }
+}
+
+// fizzBuzz(3)
+
+function ternaryFizzBuzz(num){
+    num % 3 === 0 && num % 5 === 0 ?
+    console.log("FizzBuzz")
+    :
+    num % 3 === 0 && num % 5 !== 0 ?
+    console.log("Fizz")
+    :
+    num % 5 === 0 && num % 3 !== 0 ?
+    console.log("Buzz")
+    :
+    console.log("no fizzbuzz for you")
+}
+
+ternaryFizzBuzz(15)
